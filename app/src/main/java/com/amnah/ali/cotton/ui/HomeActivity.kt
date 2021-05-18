baa38a597dee8f67ff9969afec9292daaaefd2cf
zahraa resen <zaawory98@gmail.com>
2021-05-18 10:16:49 +0300

    dark night complete

--- app/src/main/java/com/amnah/ali/cotton/ui/HomeActivity.kt
+++ app/src/main/java/com/amnah/ali/cotton/ui/HomeActivity.kt
@@ -7,6 +7,6 @@ import com.amnah.ali.cotton.R
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
