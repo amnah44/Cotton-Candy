45322ba489d3f1f4cb8b9a98040e8547da17f4e4
Sahar Qais <sahar.qais.k@gmail.com>
2021-05-19 16:36:36 +0300

    add city class

--- app/src/main/java/com/amnah/ali/cotton/data/domain/City.kt
+++ app/src/main/java/com/amnah/ali/cotton/data/domain/City.kt
@@ -1,4 +1,8 @@
 package com.amnah.ali.cotton.data.domain
-
-class City {
-}
+ data class City (
+    val city: String,
+    val city_ascii : String,
+    val country : String,
+    val capital : String,
+    val population : Int
+    )
