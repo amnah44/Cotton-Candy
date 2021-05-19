d0621664b88f3b7dbff893f8adb5258e23d69bb7
Mohammed Hakeem <mh.jwaer@gmail.com>
2021-05-19 12:33:41 +0300

    Design Home Activity Sample 2

--- app/src/main/java/com/amnah/ali/cotton/ui/HomeActivity.kt
+++ app/src/main/java/com/amnah/ali/cotton/ui/HomeActivity.kt
@@ -1,21 +1,38 @@
 package com.amnah.ali.cotton.ui
 
 import android.view.LayoutInflater
+import com.amnah.ali.cotton.R
 import com.amnah.ali.cotton.databinding.ActivityHomeBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-class HomeActivity : BaseActivity<ActivityHomeBinding>() {
+class HomeActivity : BaseActivity<ActivityHomeBinding>(), OnMapReadyCallback {
     override val LOG_TAG: String = "MAIN_ACTIVITY"
     override val bindingInflater: (LayoutInflater) -> ActivityHomeBinding =
         ActivityHomeBinding::inflate
 
-    override fun setup() {
+    private lateinit var mMap: GoogleMap
 
+    override fun setup() {
+        setupMap()
     }
+
 
     override fun addCallbacks() {
     }
 
 
+    private fun setupMap() {
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        val mapFragment = supportFragmentManager
+//            .findFragmentById(R.id.mapContainer) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+    }
 }
