b9a3122d9cc1e61abc17964ed35504becefd9b78
Sahar Qais <sahar.qais.k@gmail.com>
2021-05-19 16:26:19 +0300

    add constants

--- app/src/main/java/com/amnah/ali/cotton/util/Constrants.kt
+++ app/src/main/java/com/amnah/ali/cotton/util/Constrants.kt
@@ -1,4 +1,11 @@
 package com.amnah.ali.cotton.util
 
 object Constrants {
+    object ColumnIndex{
+        const val CITY = 0
+        const val CITY_ASCII =1
+        const val  COUNTRY = 4
+        const val CAPITAL =8
+        const val  POPULATION = 9
+    }
 }
