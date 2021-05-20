7eae06577811281dcb060fe33dac9b8c09b695cb
Mohammed Hakeem <mh.jwaer@gmail.com>
2021-05-20 18:02:35 +0300

    fix splash activity

--- app/src/main/java/com/amnah/ali/cotton/ui/SplashActivity.kt
+++ app/src/main/java/com/amnah/ali/cotton/ui/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.amnah.ali.cotton.ui
 
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import com.amnah.ali.cotton.data.DataManager
 import com.amnah.ali.cotton.databinding.ActivitySplashBinding
@@ -11,29 +13,29 @@ import java.io.InputStreamReader
 
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>() {
-    override val LOG_TAG: String = "SEARCH_ACTIVITY"
+    override val LOG_TAG: String = "SPLASH_ACTIVITY"
     override val bindingInflater: (LayoutInflater) -> ActivitySplashBinding =
         ActivitySplashBinding::inflate
 
     override fun setup() {
-        parsCsvFile()
+        Handler(Looper.getMainLooper()).postDelayed({
+            parsCsvFile()
+        }, 1000)
     }
 
     private fun parsCsvFile() {
         val inputStream: InputStream = assets.open("worldcities.csv")
-        val buffer  = BufferedReader(InputStreamReader(inputStream))
+        val buffer = BufferedReader(InputStreamReader(inputStream))
         val parser = CsvParser()
-        buffer.forEachLine {city ->
+        buffer.forEachLine { city ->
             val currentCity = parser.parse(city)
             DataManager.addCity(currentCity)
-            log(city)
         }
-        log("Done")
-//        Thread.sleep(1000)
         startActivity(Intent(this, HomeActivity::class.java))
         this.finish()
     }
 
     override fun addCallbacks() {
+
     }
 }
