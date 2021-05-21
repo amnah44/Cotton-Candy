572d88928c6dcb172bc982709aa508b4d3baf38f
Amnah Ali <amnah1li@gmail.com>
2021-05-21 18:16:21 +0300

    fix problems occur in the dark theme

--- app/src/main/java/com/amnah/ali/cotton/ui/SearchActivity.kt
+++ app/src/main/java/com/amnah/ali/cotton/ui/SearchActivity.kt
@@ -1,95 +1,98 @@
 package com.amnah.ali.cotton.ui
 
 import android.annotation.SuppressLint
-import android.util.Log
 import android.view.LayoutInflater
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.core.view.isVisible
+import com.amnah.ali.cotton.R
 import com.amnah.ali.cotton.data.DataManager
 import com.amnah.ali.cotton.data.domain.City
 import com.amnah.ali.cotton.databinding.ActivitySearchBinding
 import com.amnah.ali.cotton.util.MyValueFormaatter1
 import com.amnah.ali.cotton.util.MyValueFormatter
-import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.formatter.ValueFormatter
-import java.text.DecimalFormat
 
 class SearchActivity : BaseActivity<ActivitySearchBinding>() {
     override val LOG_TAG: String = "SEARCH_ACTIVITY"
     override val bindingInflater: (LayoutInflater) -> ActivitySearchBinding =
         ActivitySearchBinding::inflate
     var listOfCountryName = mutableListOf<String>()
-    var cityListItem  = arrayListOf<String>()
+    var cityListItem = arrayListOf<String>()
     var populationList = mutableListOf<String>()
-    var adapter : ArrayAdapter<String>? =null
+    var adapter: ArrayAdapter<String>? = null
     val populationDataList = arrayListOf<BarEntry>()
     private var cityList: MutableList<City> = mutableListOf<City>()
 
     override fun setup() {
-        binding?.listView?.isVisible =false
+        binding?.listView?.isVisible = false
         cityList = DataManager.getCityList()
-        cityList.forEach{
+        cityList.forEach {
             listOfCountryName.add(it.country)
         }
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,listOfCountryName.distinct())
+        adapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfCountryName.distinct())
         binding?.listView?.adapter = adapter
 
         showChart()
     }
 
 
-fun clearLists(){
-    //clear lists
-    cityListItem.clear()
-    populationList.clear()
-}
-    fun showChart(){
-        binding?.listView?.onItemClickListener = AdapterView.OnItemClickListener { adapterView, _, i, _ ->
-           clearLists()
-            var x = cityList.filter{
-                it.country == adapterView.getItemAtPosition(i).toString()
+    fun clearLists() {
+        //clear lists
+        cityListItem.clear()
+        populationList.clear()
+    }
 
+    fun showChart() {
+        binding?.listView?.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, _, i, _ ->
+                clearLists()
+                var x = cityList.filter {
+                    it.country == adapterView.getItemAtPosition(i).toString()
+
+                }
+                x.forEach {
+                    cityListItem.add(it.city)
+                    populationList.add(it.population)
+                    //set the select country in search view
+                    binding?.searchView?.setQuery(it.country, false)
+
+                }
+                binding?.listView?.isVisible = false
+                binding?.barChart?.isVisible = true
+
+                chart()
             }
-            x.forEach{
-                cityListItem.add(it.city)
-                populationList.add(it.population)
-                //set the select country in search view
-                binding?.searchView?.setQuery(it.country, false)
-
-            }
-            binding?.listView?.isVisible = false
-            binding?.barChart?.isVisible = true
-
-            chart()
-        }
 
     }
 
 
-
-
     @SuppressLint("ClickableViewAccessibility")
     override fun addCallbacks() {
-        binding?.apply{
-            searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+        binding?.apply {
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     searchView.clearFocus()
 
-                    return if(listOfCountryName.contains(query)){
+                    return if (listOfCountryName.contains(query)) {
                         adapter?.filter?.filter(query)
 
                         true
                     } else {
-                        Toast.makeText(applicationContext,"Country not found",Toast.LENGTH_LONG).show()
+                        Toast.makeText(applicationContext, "Country not found", Toast.LENGTH_LONG)
+                            .show()
                         false
                     }
                     return true
                 }
+
                 override fun onQueryTextChange(newText: String?): Boolean {
                     listView.isVisible = true
                     barChart.isVisible = false
@@ -104,28 +107,25 @@ fun clearLists(){
     }
 
 
-
-
-fun getPopulation(){
-
+    fun getPopulation() {
 
 
 // solve the wasted data in population
-    for (i in 0 until cityListItem.size) {
-        if (populationList[i].trim().isNotEmpty()) {
-            populationDataList.add(BarEntry(i.toFloat() + 1, populationList[i].toFloat()))
-        } else {
-            populationList[i] = "0"
-            populationDataList.add(BarEntry(i.toFloat() + 1, populationList[i].toFloat()))
-            Toast.makeText(this, "the 0 in some city mean data not fond", Toast.LENGTH_SHORT)
-                .show()
+        for (i in 0 until cityListItem.size) {
+            if (populationList[i].trim().isNotEmpty()) {
+                populationDataList.add(BarEntry(i.toFloat() + 1, populationList[i].toFloat()))
+            } else {
+                populationList[i] = "0"
+                populationDataList.add(BarEntry(i.toFloat() + 1, populationList[i].toFloat()))
+                Toast.makeText(this, "the 0 in some city mean data not fond", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
+
     }
 
-}
 
-
-    /////add Chart
+    //add Chart
 
     private fun chart() {
 
@@ -143,8 +143,6 @@ fun getPopulation(){
             barChart.data = barData
             barChart.description.text = ""
         }
-
-
 
 
         val bottomAxis: XAxis = binding?.barChart!!.xAxis
