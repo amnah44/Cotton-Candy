359cb179e8a4ae03b253cd074fdeb177b2ebdeae
zahraa resen <zaawory98@gmail.com>
2021-05-21 12:07:46 +0300

    limition search class code

--- app/src/main/java/com/amnah/ali/cotton/ui/SearchActivity.kt
+++ app/src/main/java/com/amnah/ali/cotton/ui/SearchActivity.kt
@@ -8,6 +8,8 @@ import androidx.core.view.isVisible
 import com.amnah.ali.cotton.data.DataManager
 import com.amnah.ali.cotton.data.domain.City
 import com.amnah.ali.cotton.databinding.ActivitySearchBinding
+import com.amnah.ali.cotton.util.MyValueFormaatter1
+import com.amnah.ali.cotton.util.MyValueFormatter
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
@@ -25,6 +27,7 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>() {
     var cityListItem  = arrayListOf<String>()
     var populationList = mutableListOf<String>()
     var adapter : ArrayAdapter<String>? =null
+    val populationDataList = arrayListOf<BarEntry>()
     private var cityList: MutableList<City> = mutableListOf<City>()
 
     override fun setup() {
@@ -40,17 +43,18 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>() {
     }
 
 
-
+fun clearLists(){
+    //clear lists
+    cityListItem.clear()
+    populationList.clear()
+}
     fun showChart(){
         binding?.listView?.onItemClickListener = AdapterView.OnItemClickListener { adapterView, _, i, _ ->
-            //clear lists
-            cityListItem.clear()
-            populationList.clear()
+           clearLists()
             var x = cityList.filter{
                 it.country == adapterView.getItemAtPosition(i).toString()
 
             }
-            Log.i("tag", x.toString())
             x.forEach{
                 cityListItem.add(it.city)
                 populationList.add(it.population)
@@ -76,14 +80,13 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>() {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     searchView.clearFocus()
 
-                    if(listOfCountryName.contains(query)){
+                    return if(listOfCountryName.contains(query)){
                         adapter?.filter?.filter(query)
 
-                        return true
-                    }
-                    else {
+                        true
+                    } else {
                         Toast.makeText(applicationContext,"Country not found",Toast.LENGTH_LONG).show()
-                        return false
+                        false
                     }
                     return true
                 }
@@ -103,53 +106,52 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>() {
 
 
 
+fun getPopulation(){
 
+
+
+// solve the wasted data in population
+    for (i in 0 until cityListItem.size) {
+        if (populationList[i].trim().isNotEmpty()) {
+            populationDataList.add(BarEntry(i.toFloat() + 1, populationList[i].toFloat()))
+        } else {
+            populationList[i] = "0"
+            populationDataList.add(BarEntry(i.toFloat() + 1, populationList[i].toFloat()))
+            Toast.makeText(this, "the 0 in some city mean data not fond", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+}
 
 
     /////add Chart
 
     private fun chart() {
 
-        val populationDataList = arrayListOf<BarEntry>()
-
-
-// solve the wasted data in population
-        for (i in 0 until cityListItem.size) {
-            if (populationList[i].trim().isNotEmpty()) {
-                populationDataList.add(BarEntry(i.toFloat() + 1, populationList[i].toFloat()))
-            } else {
-                populationList[i] = "0"
-                populationDataList.add(BarEntry(i.toFloat() + 1, populationList[i].toFloat()))
-                Toast.makeText(this, "the 0 in some city mean data not fond", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
-
+        getPopulation()
         val barDataSet = BarDataSet(populationDataList, "no. of Population")
-        barDataSet.valueFormatter = MyValueFormatter1()
+        barDataSet.valueFormatter = MyValueFormaatter1.MyValueFormatter1()
 
         barDataSet.valueTextSize = 5f
-        //       barDataSet.setColors(*ColorTemplate.MATERIAL_COLORS)
         val barData = BarData(barDataSet)
-        val left: YAxis = binding?.barChart!!.getAxisLeft()
+        val left: YAxis = binding?.barChart!!.axisLeft
         left.axisMinimum = 0f
 
         binding?.apply {
             barChart.animateY(500)
-            //  barChart.setFitBars(true)
             barChart.data = barData
-//            barChart.setDrawValueAboveBar(false)
             barChart.description.text = ""
         }
 
 
 
 
-        val bottomAxis: XAxis = binding?.barChart!!.getXAxis()
+        val bottomAxis: XAxis = binding?.barChart!!.xAxis
 
-        binding?.barChart!!.getXAxis().setDrawGridLines(false)
+        binding?.barChart!!.xAxis.setDrawGridLines(false)
 
-        val rightYAxis: YAxis = binding?.barChart!!.getAxisRight()
+        val rightYAxis: YAxis = binding?.barChart!!.axisRight
         rightYAxis.isEnabled = false
         rightYAxis.setDrawGridLines(false)
 
@@ -160,29 +162,9 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>() {
         bottomAxis.setLabelCount(populationDataList.size, true)
         bottomAxis.position = XAxis.XAxisPosition.BOTTOM
 
-        bottomAxis.valueFormatter = MyValueFormatter(cityListItem)
+        bottomAxis.valueFormatter = MyValueFormatter.MyValueFormatter(cityListItem)
 
 
-    }
-    class MyValueFormatter(val xValsDateLabel: ArrayList<String>) : ValueFormatter() {
-        override fun getFormattedValue(value: Float): String {
-            return value.toString()
-        }
-        override fun getAxisLabel(value: Float, axis: AxisBase): String {
-            if (value.toInt() >= 0 && value.toInt() <= xValsDateLabel.size - 1) {
-                return xValsDateLabel[value.toInt()]
-            } else {
-                return ("").toString()
-            }
-        }
-    }
-
-    class MyValueFormatter1 : ValueFormatter() {
-        private val format = DecimalFormat("#.##")
-
-        override fun getBarLabel(barEntry: BarEntry?): String {
-            return format.format(barEntry?.y)
-        }
     }
 
 
