8a0e93108be26516e221e1f4b6637c7dd1398c36
Amnah Ali <amnah1li@gmail.com>
2021-05-25 08:10:56 +0300

    update fragment name

--- app/src/main/java/com/amnah/ali/cotton/fragments/MapsFragments.kt
+++ app/src/main/java/com/amnah/ali/cotton/fragments/MapsFragments.kt
@@ -14,6 +14,6 @@ class MapsFragments : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        return inflater.inflate(R.layout.fragments_maps, container, false)
     }
 }
