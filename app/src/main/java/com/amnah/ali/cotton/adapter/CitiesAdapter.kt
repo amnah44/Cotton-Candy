24b9a9612d57f08acb17f7aa8b4cf0f6b3e1cff5
zahraa resen <zaawory98@gmail.com>
2021-05-27 11:08:32 +0300

    expand cardview

--- app/src/main/java/com/amnah/ali/cotton/adapter/CitiesAdapter.kt
+++ app/src/main/java/com/amnah/ali/cotton/adapter/CitiesAdapter.kt
@@ -1,8 +1,10 @@
 package com.amnah.ali.cotton.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.amnah.ali.cotton.R
 import com.amnah.ali.cotton.data.domain.City
 import com.amnah.ali.cotton.databinding.RawItemBinding
 
@@ -17,6 +19,7 @@ class CitiesAdapter(val list: ArrayList<City>) :
             false
         )
         return ViewHolder(view)
+
     }
     //show all components in cardView
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -26,7 +29,20 @@ class CitiesAdapter(val list: ArrayList<City>) :
             populationBox.text = list[position].population
             longitude.text = list[position].lng
             latitude.text = list[position].lat
+            //get the population percentage to progress bar
+
+            //expand cardview
+            expand.setOnClickListener {
+                if(layerHidden.visibility==View.GONE){
+                   layerHidden.visibility=View.VISIBLE
+                expand.setBackgroundResource(R.drawable.ickeyboard_arrow_up)}
+              else{
+                    layerHidden.visibility=View.GONE
+                expand.setBackgroundResource(R.drawable.ic_keyboard_arrow)
+              }
+            }
         }
+
     }
     //to get size of list
     override fun getItemCount() = list.size
