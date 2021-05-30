310ea7c23c5ec5d4e1f056413a5efa2bf5f64025
zahraa resen <zaawory98@gmail.com>
2021-05-30 10:32:51 +0300

    change base activity to base fragment

--- app/src/main/java/com/amnah/ali/cotton/fragments/SearchFragment.kt
+++ app/src/main/java/com/amnah/ali/cotton/fragments/SearchFragment.kt
@@ -19,40 +19,27 @@ import com.google.android.material.chip.ChipDrawable
 import java.util.*
 
 
-class SearchFragment : Fragment() {
+class SearchFragment :BaseFragment<FragmentSearchBinding>() {
     private var _populationList= mutableListOf<Int>()
     var avg=0.0
+    override val LOG_TAG: String="search_log"
+    override val bindingInflater: (LayoutInflater) -> FragmentSearchBinding=FragmentSearchBinding::inflate
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+
+    override fun addCallBack() {
+        changeVisibility(false)
     }
-
-    lateinit private var binding: FragmentSearchBinding
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val binded = FragmentSearchBinding.bind(view)
-        binding = binded
-        addCallbacks()
-
-
-    }
-
-
-    fun addCallbacks() {
+    override fun setup() {
         binding!!.apply {
             searchViewCountry.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
                 override fun onQueryTextSubmit(query: String?): Boolean {
-                       cardError.visibility = View.GONE
-                        changeVisibility(true)
-                       createChips(query!!.lowercase(Locale.getDefault()))
+                    cardError.visibility = View.GONE
+                    changeVisibility(true)
+                    createChips(query!!.lowercase(Locale.getDefault()))
 //                    getDataOfCountry(query!!.lowercase(Locale.getDefault()))
-                       if(query.isNullOrEmpty() || DataManager.getCurrentCountry(query)[query].isNullOrEmpty())
-                             cardError.visibility = View.VISIBLE
+                    if(query.isNullOrEmpty() || DataManager.getCurrentCountry(query)[query].isNullOrEmpty())
+                        cardError.visibility = View.VISIBLE
 
 
                     return false
@@ -83,7 +70,7 @@ class SearchFragment : Fragment() {
                     it.setTextColor(Color.BLACK)
                     it.setChipBackgroundColorResource(R.color.white)
                     it.setOnCloseIconClickListener {
-                        binding.chipsCities.removeView(it)
+                        binding?.chipsCities?.removeView(it)
                     }
                     it.text = itForCity.city
                     //ADD THE SUM OF POPULATION
@@ -91,7 +78,7 @@ class SearchFragment : Fragment() {
                     if (itForCity.population.trim().isNotEmpty()) {
                         _populationList.add(itForCity.population.toInt())
                     }
-                    binding.chipsCities.addView(it)
+                    binding?.chipsCities?.addView(it)
                 }
             }
             else{
@@ -107,13 +94,13 @@ class SearchFragment : Fragment() {
         _populationList.size.lazyLog()
 
         var sum=  _populationList!!.sum()//sum
-        binding.sumPop.text= sum.toString().chunked(3).joinToString (",")
+        binding?.sumPop?.text= sum.toString().chunked(3).joinToString (",")
 //        var max : Int? =   _populationList.maxOrNull()//max
 //        var min : Int? =   _populationList.minOrNull()//min
 //        var showMax=   avg(sum.toDouble(),max!!.toDouble())
 //        var showMin= avg(sum.toDouble(),min!!.toDouble())
            var percentage= avg(sum.toDouble())
-       percentage.toInt().let { binding.minProgressBar?.setProgressWithAnimation(it.toFloat(),1000) }
+       percentage.toInt().let { binding?.minProgressBar?.setProgressWithAnimation(it.toFloat(),1000) }
 //       showMax.toInt().let { binding.maxProgressBar?.setProgressWithAnimation(it.toFloat(),1000) }
         percentage=String.format("%.3f", percentage).toDouble()
         binding?.txtPercentage?.text = "$percentage %"
@@ -121,7 +108,7 @@ class SearchFragment : Fragment() {
     }
 
  fun changeVisibility( state:Boolean){
-     binding.apply {
+     binding?.apply {
          txtNote.isVisible=state
          txtPercentage.isVisible=state
          txtPop.isVisible=state
@@ -146,5 +133,7 @@ class SearchFragment : Fragment() {
         Log.i(tag, toString())
         return this
     }
+
+
 
 }
